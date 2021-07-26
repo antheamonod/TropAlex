8d9adfdab669af590db56d558cb769956dafaad8
Anthea Monod <23176951+antheamonod@users.noreply.github.com>
2021-07-26 13:06:36 +0300

    Create experiments_n.R

---
+++ Sampling_Triangles/experiments_n.R
@@ -0,0 +1,21 @@
+library(hitandrun)
+n <- 2
+set.seed(25)
+constr <- simplexConstraints(3*n)
+state <- har.init(constr)
+N <- 10000
+result <- har.run(state, n.samples=N)
+curvaturesN = c()
+for (i in 1:N){
+  a = c(0,result$sample[i,1:n])
+  b = c(0,result$sample[i,(n+1):(2*n)])
+  c = c(0,result$sample[i,(2*n+1):(3*n)])
+  P = matrix(c(a,b,c),nrow=length(a))
+  cv = curvatureN(P)
+  curvaturesN = c(curvaturesN, cv)
+}
+100*sum(curvaturesN==0)/length(curvaturesN) 
+100*sum(curvaturesN==1)/length(curvaturesN) 
+100*sum(curvaturesN==-1)/length(curvaturesN) 
+100*sum(curvaturesN=="undefined")/length(curvaturesN) 
+
